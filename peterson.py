36707fe8cb4babe44e795abc0ba946962035db6a
Sungryeong Park <dean0112@daum.net>
2024-05-31 17:14:52 +0900

    feat : pererson complete

--- peterson.py
+++ peterson.py
@@ -1,5 +1,7 @@
 import threading
+import time
 
+tmp = 111
 turn = 0
 flag = [False, False]
 
@@ -7,9 +9,13 @@ def function_i():
     while True:
         flag[0] = True
         turn = 1
-
+        print(0)
         while (flag[1] and turn == 1):
-            print("thread i is starting")
+            time.sleep(1)
+            print("thread i is starting\n")
+            tmp = 111
+            print("%d\n" %tmp)
+            print("thread i is done\n")
 
         flag[0] = False
 
@@ -17,10 +23,13 @@ def function_j():
     while True:
         flag[1] = True
         turn = 0
-
+        print(1)
         while(flag[0] and turn == 0):
-
-            print("thread j is starting")
+            time.sleep(1)
+            print("thread j is starting\n")
+            tmp = 222
+            print("%d\n" %tmp)
+            print("thread j is done\n")
 
         
         flag[1] = False
